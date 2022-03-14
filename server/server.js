2e261aff0fe1da3ffe980b6e164c77fdb73ebb5c
Nick Cho <nicholas.cho@hotmail.ca>
2022-03-14 09:38:37 -0400

    Setting up server

---
+++ server/server.js
@@ -0,0 +1,26 @@
+const express = require('express');
+const mongoose = require('mongoose');
+const cors = require('cors');
+const morgan = require('morgan');
+
+require('dotenv').config()
+
+const app = express();
+
+//db
+mongoose.connect(process.env.DATABASE_URL, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+})
+.then(()=>{console.log("DB connected")})
+.catch((err)=> console.log("DB connection error: ", err))
+
+//middlewares
+app.use(express.json({limit: '5mb'}))
+
+app.use(express.urlencoded({extended: true}));
+app.use(cors({origin: ['https://localhost:3000']})
+)
+
+const port = process.env.PORT || 8000;
+app.listen(port, () => console.log(`Server running on port ${port}`));
